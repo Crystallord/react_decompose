3caa35a8ba402eeadfc78374c345ecedbf1dc1cf
Sasha Havryliuk <akwaccc@gmail.com>
2021-08-30 11:39:38 +0300

    Updates

---
+++ precache-manifest.b1fa756b9026b5da1c7bf404954b6742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff696d672e7c4c3b8a4be4157f170f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d035b9668e92ad7e010",
+    "url": "./static/css/main.82746874.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d035b9668e92ad7e010",
+    "url": "./static/js/main.added7fc.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
